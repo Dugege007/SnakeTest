9bf9d0e6f89ee7178ad3177cf29b74f5a2ca4934
Dugege007 <351080175@qq.com>
2024-03-25 19:38:11 +0800

    蛇身准备 食物准备

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeTest
+{
+    public class Food : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Snake"))
+            {
+                Destroy(gameObject);
+
+                // »√…ﬂ±‰≥§
+            }
+        }
+    }
+}
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -2,42 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Snake : MonoBehaviour
+namespace SnakeTest
 {
-    public float MoveTimeInterval = 1f;
-    private float moveTimer;
-    private Vector3 currentDir;
-
-    private void Update()
+    public class Snake : MonoBehaviour
     {
-        moveTimer += Time.deltaTime;
+        public GameObject SnakeBodyPrefab;
+        public Queue<GameObject> SnakeBodyQueue = new Queue<GameObject>();
 
-        SwitchDir();
+        public float MoveTimeInterval = 1f;
+        private float moveTimer;
+        private Vector3 currentDir;
 
-        if (moveTimer > MoveTimeInterval)
+        private void Start()
         {
-            Move();
-            moveTimer = 0;
-        }
-    }
-
-    private void SwitchDir()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        if (horizontal > 0)
-            currentDir = Vector3.right;
-        if (horizontal < 0)
-            currentDir = Vector3.left;
-        if (vertical > 0)
             currentDir = Vector3.up;
-        if (vertical < 0)
-            currentDir = Vector3.down;
-    }
+        }
 
-    private void Move()
-    {
-        transform.position += currentDir;
+        private void Update()
+        {
+            moveTimer += Time.deltaTime;
+
+            SwitchDir();
+
+            if (moveTimer > MoveTimeInterval)
+            {
+                Move();
+                moveTimer = 0;
+            }
+        }
+
+        private void SwitchDir()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            if (horizontal > 0)
+                currentDir = Vector3.right;
+            if (horizontal < 0)
+                currentDir = Vector3.left;
+            if (vertical > 0)
+                currentDir = Vector3.up;
+            if (vertical < 0)
+                currentDir = Vector3.down;
+        }
+
+        private void Move()
+        {
+            // 移动前，将当前位置给到更新的蛇身
+            if (SnakeBodyQueue.Count > 0)
+            {
+                GameObject lastBody = SnakeBodyQueue.Dequeue();
+                lastBody.transform.position = transform.position;
+                SnakeBodyQueue.Enqueue(lastBody);
+            }
+
+            // 移动
+            transform.position += currentDir;
+        }
+
+        private void GrowUp()
+        {
+            GameObject bodyPrefab = Instantiate(SnakeBodyPrefab, new Vector3(1000, 1000, 0), Quaternion.identity);
+            bodyPrefab.transform.parent = transform;
+            SnakeBodyQueue.Enqueue(bodyPrefab);
+        }
     }
 }
