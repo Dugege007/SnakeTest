75b4def5e747b0f7312816c3783702d396c1262d
Dugege007 <351080175@qq.com>
2024-03-25 22:22:57 +0800

    游戏结束 最高分存储

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace SnakeTest
 {
@@ -10,20 +11,50 @@ namespace SnakeTest
         public bool IsGaming = true;
 
         public int Score = 0;
+        public int TopScore = 0;
+        public bool IsNewRecord = false;
 
         protected override void Awake()
         {
             base.Awake();
 
+            Load();
+
             if (Snake == null)
                 Snake = GameObject.FindGameObjectWithTag("Snake").GetComponent<Snake>();
         }
 
+        private void Update()
+        {
+            if (IsGaming == false)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene("Game");
+                }
+            }
+        }
+
         public void GameOver()
         {
-            //TODO
             IsGaming = false;
-            Debug.Log("ÓÎÏ·½áÊø");
+            Save();
+            GameUI.Instance.GameOver();
+        }
+
+        public void Save()
+        {
+            if (Score > TopScore)
+            {
+                TopScore = Score;
+                IsNewRecord = true;
+                PlayerPrefs.SetInt("TopScore", TopScore);
+            }
+        }
+
+        public void Load()
+        {
+            TopScore = PlayerPrefs.GetInt("TopScore", 0);
         }
     }
 }
--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -4,21 +4,44 @@ namespace SnakeTest
 {
     public class GameUI : MonoSingleton<GameUI>
     {
+        private GameManager gameManager;
+
+        public Image GameOverPanel;
+        private bool isGameOver = false;
+
         public Text ScoreText;
         private int tempScore;
+        public Text TopScoreText;
+        public Text NewRecordText;
 
         private void Start()
         {
-           
+            gameManager = GameManager.Instance;
+
+            GameOverPanel.gameObject.SetActive(false);
+            NewRecordText.gameObject.SetActive(false);
         }
 
         private void Update()
         {
-            if (GameManager.Instance.Score != tempScore)
+            if (gameManager == null || isGameOver) return;
+
+            if (gameManager.Score != tempScore)
             {
-                ScoreText.text = GameManager.Instance.Score.ToString();
-                tempScore = GameManager.Instance.Score;
+                ScoreText.text = gameManager.Score.ToString();
+                tempScore = gameManager.Score;
             }
+        }
+
+        public void GameOver()
+        {
+            isGameOver = true;
+
+            GameOverPanel.gameObject.SetActive(true);
+            TopScoreText.text = gameManager.TopScore.ToString();
+
+            if (gameManager.IsNewRecord)
+                NewRecordText.gameObject.SetActive(true);
         }
     }
 }
